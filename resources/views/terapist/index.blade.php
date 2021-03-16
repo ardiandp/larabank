e411de24a9ac11b3f88deca8764788a1edc8e36b
dianmustika <mustikacare@gmail.com>
2021-03-16 16:43:48 +0700

    tinggal tambah edit dan hapus

--- resources/views/terapist/index.blade.php
+++ resources/views/terapist/index.blade.php
@@ -140,11 +140,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
+
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1>Data Terapist</h1>
           </div>
           <div class="col-sm-6">
+
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Data Transaksi</li>
@@ -160,11 +162,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <div class="col-12">
             <div class="card">
               <div class="card-header">
-               
+               <a href="" class="btn btn-sm btn-success">Tambah Terapist </a> 
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
+                <table id="example2" class="table table-bordered table-striped">
                   <thead>
                   <tr>
                     <th>NIP</th>
@@ -249,6 +251,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
 </html>
 
 
+
+
 <!-- page script -->
 <script>
   $(function () {
@@ -258,12 +262,21 @@ scratch. This page gets rid of all links and provides the needed markup only.
     });
     $('#example2').DataTable({
       "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
+      "lengthChange": true,
+      "searching": true,
+      "ordering": false,
       "info": true,
-      "autoWidth": false,
+      "autoWidth": true,
       "responsive": true,
+
+      dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+
     });
   });
 </script>
